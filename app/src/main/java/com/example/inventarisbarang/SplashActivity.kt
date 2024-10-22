d839620f32c5562af8059e7c23999730806c298d
mwhabibi <mohammadwalidhabibi@gmail.com>
2024-10-22 15:17:21 +0700

    Commit Update Splash Screen

--- app/src/main/java/com/example/inventarisbarang/SplashActivity.kt
+++ app/src/main/java/com/example/inventarisbarang/SplashActivity.kt
@@ -5,12 +5,12 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-class HomeActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     private lateinit var buttonNext: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.home_activity) // Pastikan ini mengarah ke layout yang benar
+        setContentView(R.layout.activity_splash) // Pastikan ini mengarah ke layout yang benar
 
         // Inisialisasi tombol
         buttonNext = findViewById(R.id.button_next)
