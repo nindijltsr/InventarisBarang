ab14ae097e95fe4fd2aafb6fc5727f44b6e89106
LAPTOP-TGGLPN9K\User <nazalasyifa275@gmail.com>
2024-10-22 13:33:00 +0700

    addHomeActivity

---
+++ app/src/main/java/com/example/inventarisbarang/HomeActivity.kt
@@ -0,0 +1,25 @@
+package com.example.inventarisbarang
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity() {
+    private lateinit var buttonNext: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_activity) // Pastikan ini mengarah ke layout yang benar
+
+        // Inisialisasi tombol
+        buttonNext = findViewById(R.id.button_next)
+
+        // Set listener untuk tombol Next
+        buttonNext.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Optional: menutup HomeActivity agar tidak bisa kembali ke halaman ini
+        }
+    }
+}
