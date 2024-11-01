80e4dc7722315b2fa45efa8c359d1d0b24a6eaa1
nindijltsr <laptopindo28@gmail.com>
2024-11-01 23:03:17 +0700

    notifyItemChanged

--- app/src/main/java/com/example/inventarisbarang/BarangAdapter.kt
+++ app/src/main/java/com/example/inventarisbarang/BarangAdapter.kt
@@ -20,17 +20,23 @@ class BarangAdapter(
 
     // Inner class ViewHolder untuk mengelola tampilan item di RecyclerView
     inner class BarangViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val namaTextView: TextView = itemView.findViewById(R.id.nama_text_view) // TextView untuk nama barang
-        val kategoriTextView: TextView = itemView.findViewById(R.id.kategori_text_view) // TextView untuk kategori barang
-        val jumlahTextView: TextView = itemView.findViewById(R.id.jumlah_text_view) // TextView untuk jumlah barang
-        val buttonDelete: Button = itemView.findViewById(R.id.button_delete) // Tombol untuk menghapus barang
-        val buttonEdit: Button = itemView.findViewById(R.id.button_edit) // Tombol untuk mengedit barang
+        val namaTextView: TextView =
+            itemView.findViewById(R.id.nama_text_view) // TextView untuk nama barang
+        val kategoriTextView: TextView =
+            itemView.findViewById(R.id.kategori_text_view) // TextView untuk kategori barang
+        val jumlahTextView: TextView =
+            itemView.findViewById(R.id.jumlah_text_view) // TextView untuk jumlah barang
+        val buttonDelete: Button =
+            itemView.findViewById(R.id.button_delete) // Tombol untuk menghapus barang
+        val buttonEdit: Button =
+            itemView.findViewById(R.id.button_edit) // Tombol untuk mengedit barang
     }
 
     // Fungsi untuk membuat ViewHolder baru saat RecyclerView membutuhkan item baru
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BarangViewHolder {
         // Inflate layout XML ke dalam View dan menggunakannya untuk ViewHolder
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.barang_item, parent, false)
+        val itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.barang_item, parent, false)
         return BarangViewHolder(itemView) // Mengembalikan instance ViewHolder baru
     }
 
@@ -39,17 +45,21 @@ class BarangAdapter(
     override fun onBindViewHolder(holder: BarangViewHolder, position: Int) {
         val currentBarang = barangList[position] // Mendapatkan barang berdasarkan posisi
         holder.namaTextView.text = currentBarang.nama // Mengatur nama barang pada TextView
-        holder.kategoriTextView.text = "Kategori: ${currentBarang.kategori}" // Mengatur kategori barang
-        holder.jumlahTextView.text = "Jumlah: ${currentBarang.jumlah} Unit" // Mengatur jumlah barang
+        holder.kategoriTextView.text =
+            "Kategori: ${currentBarang.kategori}" // Mengatur kategori barang
+        holder.jumlahTextView.text =
+            "Jumlah: ${currentBarang.jumlah} Unit" // Mengatur jumlah barang
 
         holder.itemView.setOnClickListener {
             onItemClickListener(currentBarang) // Callback untuk item click
         }
         holder.buttonDelete.setOnClickListener {
             viewModel.deleteBarang(currentBarang) // Menghapus barang melalui ViewModel
+            notifyItemRemoved(position)
         }
         holder.buttonEdit.setOnClickListener {
             editClickListener(currentBarang) // Callback untuk tombol edit
+            notifyItemChanged(position)
         }
     }
 
@@ -57,9 +67,24 @@ class BarangAdapter(
     override fun getItemCount() = barangList.size
 
     // Fungsi untuk memperbarui data barang di RecyclerView
-    @SuppressLint("NotifyDataSetChanged") // Menonaktifkan peringatan karena menggunakan notifyDataSetChanged()
+//    @SuppressLint("NotifyDataSetChanged") // Menonaktifkan peringatan karena menggunakan notifyDataSetChanged()
     internal fun setBarang(barangs: List<Barang>) {
+        val oldList = this.barangList
         this.barangList = barangs // Mengatur daftar barang yang baru
-        notifyDataSetChanged() // Memberitahu RecyclerView bahwa data berubah
+
+        for (i in barangs.indices) {
+            if (oldList.size > i) {
+                if (oldList[i] != barangs[i]) {
+                    notifyItemChanged(i)
+                }
+            } else {
+                notifyItemInserted(i)
+            }
+        }
+        if (oldList.size > barangs.size) {
+            for (i in barangs.size until oldList.size) {
+                notifyItemRemoved(i)
+            }
+        }
     }
 }
